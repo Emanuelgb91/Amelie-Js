30904f9bf137d517d5f9bd935a9bb5bb6c279f70
Emanuel Gonzalez Bergonzi <emanuelgonzalez19@yahoo.com.ar>
2022-07-27 21:14:19 -0300

    Manipulacion Dom y Eventos

--- index.js
+++ index.js
@@ -1,85 +1,73 @@
-const clickButton = document.querySelectorAll('.btnAgregar');
+const buttons = document.querySelectorAll('.agregar');
 const tbody = document.querySelector('.tbody')
 let carrito = []
-
-clickButton.forEach(btn =>{
-    btn.addEventListener('click', agregarAlCarrito)
-});
+buttons.forEach(
+    function (button){
+        button.addEventListener('click', function(e){agregarAlCarrito(e)})
+    }
+)
 
 function agregarAlCarrito(e){
     const button = e.target
     const item = button.closest(".tarjeta")
     const itemTitulo = item.querySelector('.tarjeta__titulo').textContent;
     const itemPrecio = item.querySelector('.precio').textContent;
-    const itemImg = item.querySelector('.tarjeta__imagen').src;
-    
+        
     const nuevoProducto = {
         titulo: itemTitulo,
         precio: itemPrecio,
-        imagen: itemImg,
+        id: `${Math.random()*1000}`,
         cantidad: 1
     }
-    agregarItemAlCarrito(nuevoProducto)
-}
-
-function agregarItemAlCarrito(nuevoProducto) {
-    const inputElemento = tbody.getElementsByClassName('input__elemento')
-    for (let i = 0; i < carrito.length; i++) {
-        if (carrito[i].titulo.trim() === nuevoProducto.titulo.trim()) {
-            carrito[i].cantidad ++;
-            const inputValor = inputElemento[i]
-            inputValor.value++
-            totalCarrito()
-            return null;
-        }
-        
-    }
     carrito.push(nuevoProducto);
-    renderCarrito()
+    renderCarrito();
 }
 
-function renderCarrito(){
+function renderCarrito() {
     tbody.innerHTML = ''
     carrito.map (item =>{
-    const tr = document.createElement('tr')
-    tr.classList.add('itemCarrito')
-    const contenido = `
-    <th scope="row">1</th>
-    <td class="tabla__producto">
-    <img src=${item.img} alt="">
-    <h6 class="titulo__imagen">${item.titulo}</h6></td>
-    <td class="tabla__precio"><p>$ ${item.precio}</p></td>
-    <td class="tabla__cantidad"><input type="number" min="1" value=${item.cantidad} class="input__elemento">
-    <button class="delete btn btn-danger">X</button></td>
-    `
+        const tr = document.createElement('tr')
+        const contenido = `
+            <td class="tabla__producto">
+            <h6 class="titulo__imagen">${item.titulo}</h6></td>
+            <td class="tabla__precio"><p>$ ${item.precio}</p></td>
+            <td class="tabla__cantidad"><input type="number" min="1" value=${item.cantidad} class="input__elemento">
+            <button class="delete btn btn-danger">X</button></td>
+        `
 
         tr.innerHTML = contenido
-        tbody.append(tr)
-        // tr.querySelector('.delete').addEventListener('click', removerItemCarrito())
+        tbody.appendChild(tr)
+        tr.querySelector('.delete').addEventListener('click', () => removerItemCarrito(item.id));
+        tr.querySelector('.input__elemento').addEventListener('change', (event) => sumarLaCantidad(item.id, event) )
     })
-    
+    totalCarrito()
 }
 
 function totalCarrito(){
     let total = 0
     const totalItemCarrito = document.querySelector('.totalCarrito');
     carrito.forEach((item) => {
-        const precio = Number(item.precio.replace("$", ''))
-        total = total + precio * item.cantidad
+        const precioProducto = Number(item.precio);
+        total = total + precioProducto * item.cantidad
     })
 
-    totalItemCarrito.innerHTML = `total ${total}`
+    totalItemCarrito.innerHTML = `total $${total}`
 }
 
-// function removerItemCarrito(e){
-//     const botonEliminar = e.target
-//     const tr = botonEliminar.closest('.itemCarrito')
-//     const titulo = tr.querySelector('.__titulo').textContent;
-//     for (let i=0 ; i < carrito.length ; i++){
-//         if (carrito[i].titulo.trim() === titulo.trim()) {
-//             carrito.splice(i, 1)
-//         }
-//     }
-//     tr.remove()
-//     totalCarrito()
-// }
+function removerItemCarrito(id){
+    let nuevoCarrito = []
+    nuevoCarrito = carrito.filter((item) => (item.id !== id ))
+    carrito = nuevoCarrito
+    renderCarrito()
+    
+}
+
+function sumarLaCantidad(id, event){
+    const nuevoCarrito = carrito
+    let nuevaCantidad = Number(event.target.value);
+    let indiceItem = carrito.findIndex((item) => (item.id === id))
+    nuevoCarrito[indiceItem].cantidad = nuevaCantidad
+    carrito = nuevoCarrito
+    renderCarrito()
+    
+}
